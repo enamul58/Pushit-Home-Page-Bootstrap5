6308e2f6526f2d8e57b324b58ca9eff87109d718
“enamul58” <enamullimon@gmail.com>
2022-08-24 05:55:41 +0600

    2nd commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
-$(function () {
-  $("[data-bs-target]").on("click", function () {
-    var trageet_id = $(this).attr("data-bs-target");
-    $(trageet_id).toggleClass("show");
-    $("body").toggleClass("offcanvas-active");
-  });
-});
+// $(document).ready(function () {
+//   $("[data-bs-trigger]").on("click", function () {
+//     var trageet_id = $(this).attr("data-bs-trigger");
+//     $(trageet_id).toggleClass("show");
+//     // $("body").toggleClass("offcanvas-active");
+//   });
+
+// $(".fa-times").click(function (e) {
+//   alert("enaml");
+//   $("#navbarNav").removeClass("show");
+//   // $("body").removeClass("offcanvas-active");
+// });
+//   $(".fa-times").click(function (e) {
+//     e.preventDefault();
+//     alert("The paragraph was clicked.");
+//   });
+// });
+
+// jQuery(document).ready(function ($) {
+//   $(".menu-toggle").on("click", function () {
+//     $("#mob-navbar").toggleClass("navbar-collapse");
+//     // $(".overlay").toggleClass("menu-open");
+//   });
+
+//   $(".fa-times").on("click", function () {
+//     $("#mob-navbar").removeClass("navbar-collapse");
+//   });
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   $("button").on("click", function () {
+//     alert("enamul");
+//   });
+// });
